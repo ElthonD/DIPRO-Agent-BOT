2fa81da1404c996454cabb04d594e4625e8cde3c
ElthonD <elthon.rivas@gmail.com>
2025-04-30 17:10:22 -0600

    ajustes data

--- analisis.py
+++ analisis.py
@@ -51,7 +51,7 @@ torch.classes.__path__ = []
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Rutas relativas portables
-DATA_PATH = os.path.join(BASE_DIR, "Data", "RFI Data.xlsx")
+DATA_PATH = os.path.join(BASE_DIR, "Data", "Data RFI.xlsx")
 LOGO_PATH = os.path.join(BASE_DIR, "Imagenes", "Dipro_Logo1.png")
 MODEL_PATH = os.path.join(BASE_DIR, "modelo.pkl")
 
@@ -61,11 +61,31 @@ MODEL_PATH = os.path.join(BASE_DIR, "modelo.pkl")
 
 def createPage():
     
+    
+    ############################################
+    # Función para extraer las iniciales
+    ############################################
+
+    def obtener_iniciales(texto):
+        if pd.isna(texto):
+            return ''
+        palabras = texto.strip().split()
+        iniciales = ''.join([palabra[0].upper() for palabra in palabras if palabra])
+        return iniciales
+    
     # Función para cargar datos desde Excel (usando caché de Streamlit)
     @st.cache_data(show_spinner='Cargando Datos... Espere...', persist=True)
     def load_df():
         df = pd.read_excel(DATA_PATH)
-        df['Fecha/Hora de creación'] = pd.to_datetime(df['Fecha/Hora de creación'])
+        #df['Fecha/Hora de creación'] = pd.to_datetime(df['Fecha/Hora de creación'])
+        # Damos drop a la columnas "Unnamed:5 hasta 9"
+        df = df.drop(columns=['Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9'])
+        # Eliminar retornos de carro y saltos de línea del dataframe "_x000D_\n"
+        df = df.replace(to_replace=r'(\r\n|\r|\n|_x000D_\\n|_x000D_)', value=' ', regex=True)
+        # Quitar espacios duplicados
+        df = df.applymap(lambda x: ' '.join(str(x).split()) if isinstance(x, str) else x)
+        # Aplicar la función a una nueva columna
+        df['ID'] = df['Formato'].apply(obtener_iniciales)
         # Si existe la columna "ID", se renombra a "Registro" y se usa como identificador;
         # de lo contrario, se utiliza el índice.
         if 'ID' in df.columns:
@@ -79,18 +99,75 @@ def createPage():
     try:
         data = load_df()
     except Exception as e:
-        st.error("Error al cargar el archivo 'RFI Data.xlsx'.")
+        st.error("Error al cargar el archivo 'Data RFI.xlsx'.")
         st.error(str(e))
         return
-
-    st.markdown("<h3 style='text-align: left;'>Data</h3>", unsafe_allow_html=True)
-    st.dataframe(data)
     
+    #############################################
+    # Función para Diagrama de Pareto (80-20)
+    ############################################
+    
+    def diagrama_pareto(df):
+        
+        # Calcular la frecuencia de cada valor en la columna 'Área'
+        conteo = df['Formato'].value_counts().reset_index()
+        conteo.columns = ['Formato', 'Frecuencia']
+
+        # Ordenar de mayor a menor frecuencia
+        conteo = conteo.sort_values(by='Frecuencia', ascending=False)
+
+        # Calcular la suma acumulada y el porcentaje acumulado
+        conteo['Acumulado'] = conteo['Frecuencia'].cumsum()
+        total = conteo['Frecuencia'].sum()
+        conteo['Porcentaje Acumulado'] = 100 * conteo['Acumulado'] / total
+
+        # Crear la figura de Plotly
+        fig = go.Figure()
+
+        # Agregar el gráfico de barras para la frecuencia
+        fig.add_trace(go.Bar(
+            x=conteo['Formato'],
+            y=conteo['Frecuencia'],
+            name='Frecuencia',
+            text=conteo["Frecuencia"],
+            textposition="outside",
+        ))
+
+        # Agregar el gráfico de línea para el porcentaje acumulado
+        fig.add_trace(go.Scatter(
+            x=conteo['Formato'],
+            y=conteo['Porcentaje Acumulado'],
+            name='Porcentaje Acumulado',
+            mode='lines+markers',
+            text=conteo["Frecuencia"],
+            textposition="top center",
+            yaxis='y2'
+        ))
+
+        # Configurar el layout para tener dos ejes Y
+        fig.update_layout(
+            title='Diagrama de Pareto',
+            xaxis=dict(title='Formato'),
+            yaxis=dict(title='Frecuencia'),
+            yaxis2=dict(
+                title='Porcentaje Acumulado (%)',
+                overlaying='y',
+                side='right',
+                range=[0, 110]
+            ),
+            legend=dict(x=0.75, y=1.15)
+        )
+        
+        # Mostrar la gráfica
+        fig.show()
+
+   
+        
     ############################################
     # Funciones Auxiliares para el Preprocesado
     ############################################
 
-    def extract_common_stopwords(df, col1="Pregunta del RFI", col2="Respuesta", top_n=10):
+    def extract_common_stopwords(df, col1="Pregunta", col2="Respuesta", top_n=10):
         """
         Extrae de las columnas especificadas los tokens que ya se
         encuentran en la lista de stopwords de nltk y retorna una lista
@@ -112,7 +189,7 @@ def createPage():
         return common_stop
 
     # Extraer automáticamente las stopwords más comunes de las columnas de interés
-    common_stopwords = extract_common_stopwords(data, col1="Pregunta del RFI", col2="Respuesta", top_n=10)
+    common_stopwords = extract_common_stopwords(data, col1="Pregunta", col2="Respuesta", top_n=10)
     #st.write("Stopwords más comunes extraídas:", common_stopwords)
     
     # Función para preprocesar texto: minúsculas, tokenización y eliminación de stopwords y puntuación
@@ -126,14 +203,14 @@ def createPage():
         # Agregar las stopwords más comunes extraídas del archivo
         stop_words.update(common_stopwords)
         # También se pueden agregar palabras adicionales manualmente
-        stop_words.update(("amp", "xa", "xe", "Buenos", "días", "tardes", "noches", "Hola", "Gracias","para","la", "de", "el", "en", "a", "y", "que", "es", "se", "por", "respecto", "con", "un", "una", "los", "las", "del", "al", "este", "esta", "esto", "esto", "aquí", "ahí", "allí", "indica", "se", "han","valioso", "apoyo", "atención", "respuesta", "consultas", "duda", "pregunta", "respuesta", "solicitud", "requerimiento", "información", "proyecto", "propuesta", "documento", "anexo", "adjunto", "contratista", "referencia", "alcance", "términos", "condiciones", "contrato", "licitación", "oferta", "evaluación", "proceso", "plazo", "fecha", "hora", "reunión", "presentación", "requerido", "necesario", "sugerencia", "comentario", "observación", "darnos", "respuesta", "favor", "enviar", "adjuntar", "comunicación", "correo", "electrónico", "respuesta", "solicitud", "requerimiento", "información", "proyecto", "propuesta", "documento", "anexo", "adjunto", "contratista", "referencia", "alcance", "términos", "condiciones", "contrato", "licitación", "oferta", "evaluación", "proceso", "referente"))
+        stop_words.update(("amp", "xa", "xe", "plano", "indica", "proyecto", "si", "apoyo", "área", "favor", "acuerdo", "detalle", "solicita", "rfi", "area", "si", "existente", "buena", "oc", "cm", "aunado", "indicar", "referente", "trabajos", "tarde", "solicito", "cambio", "hallazgo", "adjunta", "producto", "nuevo", "solicitamos", "indiquen", "ser", "confirmar", "embargo", "procede", "ie", "indicarnos", "realizar", "confirmar", "procede"))
         #stop_words.update(("amp", "xa", "xe", "Buenos", "días", "tardes", "noches", ...))
         tokens = [token for token in tokens if token not in stop_words and token not in string.punctuation]
         return " ".join(tokens)
 
     # Limpieza de columnas importantes: se agregan las versiones limpias de preguntas y respuestas
     def limpieza_columnas_importantes(df):
-        df['Pregunta_clean'] = df['Pregunta del RFI'].apply(preprocess_text)
+        df['Pregunta_clean'] = df['Pregunta'].apply(preprocess_text)
         # Asegurarse de que la columna de respuestas se llame correctamente (puede ser "Respuestas" o "Respuesta")
         df['Respuesta_clean'] = df['Respuesta'].apply(preprocess_text)
         return df
@@ -229,7 +306,7 @@ def createPage():
 
     # Función para calcular la frecuencia de cada grupo y fusionarla al DataFrame
     def calculo_frecuencia_grupo(df):
-        group_preg_freq = df.groupby('Pregunta_Group_ID')['Pregunta del RFI'].count().rename('Pregunta_Frequency')
+        group_preg_freq = df.groupby('Pregunta_Group_ID')['Pregunta'].count().rename('Pregunta_Frequency')
         group_resp_freq = df.groupby('Respuesta_Group_ID')['Respuesta'].count().rename('Respuesta_Frequency')
         df = df.merge(group_preg_freq, on='Pregunta_Group_ID', how='left')
         df = df.merge(group_resp_freq, on='Respuesta_Group_ID', how='left')
@@ -237,7 +314,7 @@ def createPage():
 
     # Función para reestructurar el DataFrame final con las columnas requeridas
     def reestructurar_dataframe(df):
-        final_df = df[['Registro', 'Pregunta del RFI', 'Pregunta_Group_ID', 
+        final_df = df[['Registro', 'Pregunta', 'Pregunta_Group_ID', 
                        'Respuesta', 'Respuesta_Group_ID', 'Pregunta_Frequency', 'Respuesta_Frequency']]
         return final_df
 
@@ -324,6 +401,22 @@ def createPage():
         4. Exportar el resultado a Excel y realizar un análisis exploratorio (Pareto).
         """)
         
+        # Carga de Data
+        st.markdown("<h3 style='text-align: left;'>Data</h3>", unsafe_allow_html=True)
+        st.dataframe(data)
+        
+        # Cuantificar Áreas (Formato)
+        st.markdown("<h3 style='text-align: left;'>Cuantificar Áreas (Formato)</h3>", unsafe_allow_html=True)
+        cantidad_areas = (len(data['Formato'].unique()) - 1)
+        lista_areas = data['Formato'].unique().tolist()
+        lista = lista_areas.pop(-1)
+        st.write(f"La cantidad de áreas (Formato) presentes en el documento son:" + str(cantidad_areas) + ". Las áreas (Formato) presentes son: " + str(lista_areas) + ".")
+        
+        # Diagrama de Pareto
+        st.markdown("<h3 style='text-align: left;'>Gráfico Pareto</h3>", unsafe_allow_html=True)
+        pareto_areas1 = diagrama_pareto(data)
+
+        """
         # Aplicar limpieza a las columnas importantes
         data_limpia = limpieza_columnas_importantes(data)
 
@@ -363,9 +456,9 @@ def createPage():
         # Mostrar la gráfica en Streamlit
         #st.markdown("<h3 style='text-align: left;'>Grupos que representan el 80% de las consultas:</h3>", unsafe_allow_html=True)
         #st.plotly_chart(fig, use_container_width=True)
-
+        """
     except Exception as e:
-        st.error("Error al procesar el archivo 'RFI Data.xlsx'.")
+        st.error("Error al procesar el archivo 'Data RFI.xlsx'.")
         st.error(str(e))
         return
 
