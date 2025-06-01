a929630a587e0125bcaaf39d7226bfcbd64b3915
ElthonD <elthon.rivas@gmail.com>
2025-06-01 17:16:08 -0600

    ajs

--- download_embeddings.sh
+++ download_embeddings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 mkdir -p Models
 # Descarga con autenticación usando el token de Hugging Face
 wget --header="Authorization: Bearer $HF_TOKEN" -O Models/embeddings.joblib "https://huggingface.co/Elthon5/modelos/resolve/main/embeddings.joblib"
--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -e
 mkdir -p Models
 wget -O Models/llama-2-7b-chat.Q2_K.gguf "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf"
