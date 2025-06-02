e87da4248c193d5904192533c81f9be9661a0b3f
ElthonD <elthon.rivas@gmail.com>
2025-06-02 12:37:36 -0600

    ajustes img

--- start.py
+++ start.py
@@ -9,23 +9,20 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Rutas relativas portables
 
-LOGO_PATH = os.path.join(BASE_DIR, "Imagenes", "Dipro_Logo.png")
+LOGO_PATH = os.path.join(BASE_DIR, "Imagenes", "Dipro_Logo1.png")
 
-# Title of the main page
-pathLogo = pathLogo = r'D:\Proyectos\DIPRO Agent BOT\Imagenes\Dipro_Logo1.png'
-# Abrir imagen y convertirla a RGB (3 canales)
-    # Abrir imagen con canal alfa
-img = Image.open(LOGO_PATH).convert("RGBA")
-# Convertir a numpy array
-#display = np.array(display)
-col1, col2, col3 = st.columns([1.5,1,1])
-with col2:
-    st.image(img, width=100)
 ### App de Inicio
 
 def createPage():
     
-    
+    # Abrir imagen y convertirla a RGB (3 canales)
+        # Abrir imagen con canal alfa
+    img = Image.open(LOGO_PATH).convert("RGBA")
+    # Convertir a numpy array
+    #display = np.array(display)
+    col1, col2, col3 = st.columns([1.5,1,1])
+    with col2:
+        st.image(img, width=100)    
 
     st.markdown('Bienvenido a ***Aplicación DIPRO***, para identificar patrones semánticos en las preguntas del FDI y sus respuestas para encontrar relaciones semánticas entre los registros.')
 
