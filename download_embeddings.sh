37bff74b5f289e6ba31b926e9810048e766e5757
ElthonD <elthon.rivas@gmail.com>
2025-06-02 08:54:51 -0600

    ajustar cambios

--- download_embeddings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p Models
-# Descarga con autenticación usando el token de Hugging Face
-wget --header="Authorization: Bearer $HF_TOKEN" -O Models/embeddings.joblib "https://huggingface.co/Elthon5/modelos/resolve/main/embeddings.joblib"
-wget --header="Authorization: Bearer $HF_TOKEN" -O Models/nn_model.joblib "https://huggingface.co/Elthon5/modelos/resolve/main/nn_model.joblib"
--- download_model.sh
+++ download_model.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
 mkdir -p Models
-wget -O Models/llama-2-7b-chat.Q2_K.gguf "https://huggingface.co/Elthon5/dipro-agent-bot-embeddings/resolve/main/llama-2-7b-chat.Q2_K.gguf"
-wget -O Models/embeddings.joblib "https://huggingface.co/Elthon5/dipro-agent-bot-embeddings/resolve/main/embeddings.joblib"
-wget -O Models/nn_model.joblib "https://huggingface.co/Elthon5/dipro-agent-bot-embeddings/resolve/main/nn_model.joblib"
+wget --header="Authorization: Bearer $HF_TOKEN" -O Models/llama-2-7b-chat.Q2_K.gguf "https://huggingface.co/Elthon5/dipro-agent-bot-embeddings/resolve/main/llama-2-7b-chat.Q2_K.gguf"
+wget --header="Authorization: Bearer $HF_TOKEN" -O Models/embeddings.joblib "https://huggingface.co/Elthon5/dipro-agent-bot-embeddings/resolve/main/embeddings.joblib"
+wget --header="Authorization: Bearer $HF_TOKEN" -O Models/nn_model.joblib "https://huggingface.co/Elthon5/dipro-agent-bot-embeddings/resolve/main/nn_model.joblib"
